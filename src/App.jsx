921631a13eed2a1a0804f869464b049015f57dee
abhijeetpalanki <ajpalanki@gmail.com>
2024-06-26 18:25:50 -0500

    home page animations and structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import Home from "./pages/Home";
+import Customizer from "./pages/Customizer";
+import Canvas from "./canvas";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="transition-all ease-in app">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  );
 };
 
 export default App;
